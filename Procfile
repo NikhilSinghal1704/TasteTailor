029012a02b128e1ab2852e3b62bad52624aa621c
Nikhil Singhal <70971092+NikhilSinghal1704@users.noreply.github.com>
2023-11-02 20:49:44 +0530

    getting production ready

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn TasteTailor.wsji --log-file -
--- TasteTailor/settings.py
+++ TasteTailor/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-0qkhdoj0*t=(gyeg1z%)4k57d_56^9pxh8tik6*e=iyen)_#kw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://tion-comment-website-tests.trycloudflare.com', '*']
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = ['https://client-tap-strings-serbia.trycloudflare.com']
 
@@ -129,9 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-                    ]
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
