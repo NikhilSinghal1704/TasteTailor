b6e7ca391ff76c1372161da04cee92b8fbe3c2c7
Nikhil Singhal <70971092+NikhilSinghal1704@users.noreply.github.com>
2023-11-02 21:21:38 +0530

    Update settings.py

--- TasteTailor/settings.py
+++ TasteTailor/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-0qkhdoj0*t=(gyeg1z%)4k57d_56^9pxh8tik6*e=iyen)_#kw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://tastetailor-production.up.railway.app']
+ALLOWED_HOSTS = ['tastetailor-production.up.railway.app']
 
 CSRF_TRUSTED_ORIGINS = ['https://tastetailor-production.up.railway.app']
 
