5e544d90d1e8f1135cce7ea0e74bd320ba002590
Nikhil Singhal <70971092+NikhilSinghal1704@users.noreply.github.com>
2023-11-02 22:24:44 +0530

    lol

--- TasteTailor/info.py
+++ TasteTailor/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'TasteTailor@gmail.com'
-EMAIL_HOST_PASSWORD = 'kngnaaztwgbtrgtn'
+EMAIL_HOST_USER = 'culinaryai01@gmail.com'
+EMAIL_HOST_PASSWORD = 'mtzbrkancexwfobu'
 EMAIL_PORT = 587
