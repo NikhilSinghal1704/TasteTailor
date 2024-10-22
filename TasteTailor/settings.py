a5b7736219277bf027e7ca569f97994f99ffd6f6
Nikhil Singhal <70971092+NikhilSinghal1704@users.noreply.github.com>
2024-10-22 21:04:05 +0530

    Update settings.py

--- TasteTailor/settings.py
+++ TasteTailor/settings.py
@@ -37,7 +37,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["tastetailor.nik-server.in", "*"]
 
-CSRF_TRUSTED_ORIGINS = ["http://tastetailor.nik-server.in", "https://tastetailor.nik-server.in"]
+CSRF_TRUSTED_ORIGINS = ["https://tastetailor.nik-server.in/"]
 
 
 # Application definition
