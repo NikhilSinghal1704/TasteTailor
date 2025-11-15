71ce411125a27b2e4e8139c88155956ca43cd069
Harshit Sachdeva <harshitsachdeva291104@gmail.com>
2025-11-15 13:47:35 +0530

    contact working

--- Home/admin.py
+++ Home/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import APIKey
+from .models import APIKey, Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'created_at', 'is_read')
+    list_filter = ('is_read', 'created_at')
+    search_fields = ('name', 'email', 'subject', 'message')
+    list_editable = ('is_read',)
+    readonly_fields = ('created_at',)
+    fieldsets = (
+        ('Contact Information', {
+            'fields': ('name', 'email', 'subject', 'message')
+        }),
+        ('Status', {
+            'fields': ('is_read', 'created_at'),
+        }),
+    )
+
 
 @admin.register(APIKey)
 class APIKeyAdmin(admin.ModelAdmin):
